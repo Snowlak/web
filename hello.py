a8a6ed6000fb39d8574366d8650548d8fb32265b
Snowlak <snezanaapolonik@gmail.com>
2021-10-19 23:00:26 +0300

    change

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-def wsgi_application(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    body = bytes(environ['QUERY_STRING'].replace('&', '\n'))
-    return body
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
