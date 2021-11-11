1db3f0d864d6f3a4dfc1aebc8a22365cf23eec34
Snowlak <cnejaapolonik@gmail.com>
2021-11-12 01:02:11 +0300

    change in conf

--- etc/ask_conf.py
+++
@@ -1,2 +0,0 @@
-pythonpath = 'ask.ask.wsgi:application'
-bind = '0.0.0.0:8000'
--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
-sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn_wsgi.conf hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
