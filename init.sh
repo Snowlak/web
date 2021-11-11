2f988bc39ea39342ca56d13b9c735963c7781e77
Snowlak <cnejaapolonik@gmail.com>
2021-11-11 21:53:20 +0300

    change in conf

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn_wsgi.conf hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
+gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application
