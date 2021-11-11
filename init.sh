e74e6e8d5fe806f011e19e793eb974a68dc220ed
Snowlak <cnejaapolonik@gmail.com>
2021-11-11 19:09:03 +0300

    add django conf

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn_wsgi.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
