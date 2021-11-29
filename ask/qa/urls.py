ee317282ff9a7879ba621602816b389ba1ca9126
Snowlak <cnejaapolonik@gmail.com>
2021-11-29 20:26:42 +0300

    added html representation

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_all_new_question),
-    url(r'^login/.*$', views.test),
-    url(r'^signup/.*', views.test),
-    url(r'^ask/.*', views.test),
-    url(r'^popular/.*', views.post_all_popular_question),
-    url(r'^new/.*', views.test),
-    url(r'^question/(?P<id>[0-9]+)/$', views.one_post)
+    url(r'^login/', views.test),
+    url(r'^signup/', views.test),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.post_all_popular_question),
+    url(r'^new/', views.test),
+    url(r'^question/(?P<id>[0-9]+)', views.one_post)
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,14 +2,16 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator
 from .models import Question
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 
+@require_GET
 def post_all_new_question(request):
-    posts = Question.objects.all().order_by('-id')
+    posts = Question.objects.order_by('-id')
     try:
         page = int(request.GET.get('page'))
     except ValueError:
@@ -26,6 +28,7 @@ def post_all_new_question(request):
     })
 
 
+@require_GET
 def post_all_popular_question(request):
     try:
         page = int(request.GET.get('page'))
@@ -33,7 +36,7 @@ def post_all_popular_question(request):
         page = 1
     except TypeError:
         page = 1
-    posts = Question.objects.all().order_by('-rating')
+    posts = Question.objects.order_by('-rating')
     paginator = Paginator(posts, 10)
     page = paginator.page(page)
     return render(request, 'posts.html', {
@@ -44,8 +47,9 @@ def post_all_popular_question(request):
     })
 
 
+@require_GET
 def one_post(request, pk):
-    q = Question.objects.get_object_or_404(pk=pk)
+    q = Question.objects.get_object_or_404(pk=id)
     return render(request, 'post/one_post_page.html', {
         'post': q,
     })
